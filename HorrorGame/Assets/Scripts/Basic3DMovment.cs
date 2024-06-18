eb0ccd20cd74cd18c8a438dee76eb0fd74a841b8
KDRuizB <kevin_ruiz@web.de>
2024-06-18 20:11:24 +0200

    Added Sprinten & Springen
    Damit der Spieler springen kann braucht der Untergrund auf dem er steht den Tag "Ground"

--- HorrorGame/Assets/Scripts/Basic3DMovment.cs
+++ HorrorGame/Assets/Scripts/Basic3DMovment.cs
@@ -5,39 +5,82 @@ using UnityEngine;
 
 public class Basic3DMovment : MonoBehaviour
 {
+    // Spieler Rigidbody
     Rigidbody rb;
-    [SerializeField]float speed = 10;
-    [SerializeField]GameObject cam;
+    // Gehen
+    [SerializeField] float walkSpeed = 10;
+    // Kamera Bewegung
+    [SerializeField] GameObject cam;
+    // Sprinten
+    [SerializeField] KeyCode sprintKey = KeyCode.LeftShift; 
+    [SerializeField] float sprintSpeed = 18;
+    float currentSpeed;
+    // Springen
+    [SerializeField] KeyCode jumpKey = KeyCode.Space; 
+    [SerializeField] float jumpForce = 5;
+    // Überprüfen, ob der Spieler am Boden ist
+    bool isGrounded;
 
     void Awake()
     {
-        //Rigidbody zuweisen(Muss bei unity im gleichen Gameobject, wie der script sein)
+        // Rigidbody zuweisen (Muss bei Unity im gleichen GameObject wie das Script sein)
         rb = gameObject.GetComponent<Rigidbody>();
 
-        //Maus auf den screen locken
+        // Maus auf den Screen locken
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
-        //Roatationinputs speichern(Die x und y werte sind absichtlich verkehrtherum)
+        // Rotationinputs speichern (Die x- und y-Werte sind absichtlich verkehrtherum)
         float y = Input.GetAxis("Mouse X") * 2;
         float x = Input.GetAxis("Mouse Y") * 2;
-        //Rotation berechnen
+
+        // Rotation berechnen
         Vector3 rotateValue1 = new Vector3(0, y * -1, 0);
         Vector3 rotateValue2 = new Vector3(x, 0, 0);
         transform.eulerAngles = transform.eulerAngles - rotateValue1;
 
-        //Rotation der Cam zuweisen
+        // Rotation der Kamera zuweisen
         cam.transform.eulerAngles = cam.transform.eulerAngles - rotateValue2;
 
+        // Bewegungsinputs speichern
+        Vector3 relativeInputs = transform.TransformDirection(new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")));
 
-        //Bewegungsinputs speichern
-        Vector3 relativeInputs = transform.TransformDirection(new(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")));
+        // Prüfen, ob die Sprinttaste gedrückt ist
+        if (Input.GetKey(sprintKey))
+        {
+            currentSpeed = sprintSpeed;
+        }
+        else
+        {
+            currentSpeed = walkSpeed;
+        }
 
-        //Laufbewegung zuweisen
-        rb.velocity = new(speed * relativeInputs.x, rb.velocity.y, speed * relativeInputs.z);
+        // Laufbewegung zuweisen
+        rb.velocity = new Vector3(currentSpeed * relativeInputs.x, rb.velocity.y, currentSpeed * relativeInputs.z);
 
-        
+        // Prüfen, ob die Sprungtaste gedrückt wurde und der Spieler auf dem Boden ist
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    // Überprüfen, ob der Spieler den Boden berührt
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
     }
 }
